85145538bd242dfa0fe09de463050edf152b5610
thomas demoncy <demoncy.thomas@gmail.com>
2023-09-12 14:34:49 +0200

    finished the exercise, but heck I don't understand how.

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(()  => {
+      this.currentTime += 1;
+
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = 0;
+    seconds = Math.floor(this.currentTime % 60);
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value >= 10) {
+      return value.toString();
+    } else {
+      return ("0" + value).slice(-2)
+    }
   }
 
   stop() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
 
   reset() {
+    return this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
-    // ... your code goes here
+    return ` ${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
